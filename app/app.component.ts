db00fb448687a41e3d6e995162caff068d4a8197
Chance <ChanceMagno@gmail.com>
2017-05-05 08:48:56 -0700

    create basic shell layout for project

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+  <h1> Test </h1>
+  </div>
+`
+})
+
+
+export class AppComponent {
+
+
+}
---
+++ app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+// import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material';
+
+
+
+import { FormsModule } from '@angular/forms';
+
+// import { AngularFireModule } from 'angularfire2';
+// import { masterFirebaseConfig } from './api-keys';
+
+// export const firebaseConfig = {
+//   apiKey: masterFirebaseConfig.apiKey,
+//   authDomain: masterFirebaseConfig.authDomain,
+//   databaseURL: masterFirebaseConfig.databaseURL,
+//   storageBucket: masterFirebaseConfig.storageBucket
+// }
+
+@NgModule({
+  imports: [ BrowserModule, FormsModule, BrowserAnimationsModule, MaterialModule.forRoot(),
+],
+  declarations: [ AppComponent],
+  bootstrap: [ AppComponent ]
+})
+
+export class AppModule {}
